57cefd8953ad9ecdb80d2dc24f718c1c63a76337
Aleksandr Goncharenko <Aleks13vkok@gmail.com>
2021-04-04 16:59:18 +0300

    Open folders feature complete

--- server/Controllers/fileController.js
+++ server/Controllers/fileController.js
@@ -28,7 +28,7 @@ class FileController{
 
     async fetchFiles(req, res){
         try{
-            const files = await  File.find({user: req.user.id, parent: req.body.parent})
+            const files = await  File.find({user: req.user.id, parent: req.query.parent})
             return res.json(files)
         }catch (e) {
             console.log(e);
